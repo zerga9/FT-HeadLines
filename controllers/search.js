29abeadf92ca09e5a2dd32804152f785eb648aaa
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-15 11:13:28 +0100

    More refactoring search.js

--- controllers/search.js
+++ controllers/search.js
@@ -2,9 +2,6 @@ var express = require('express');
 var request = require('request-promise');
 var router = express.Router();
 
-const FT_API_KEY = process.env.FT_API_KEY;
-const apiUrl = "http://api.ft.com/content/search/v1?apiKey=" + FT_API_KEY;
-
 var apiActions = require('../api/index');
 
 router.get('/search', (req,res) => {
