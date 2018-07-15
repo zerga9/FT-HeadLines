3b14df5f03d8abb8e737bcb40d68600d40032ba0
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-15 10:29:55 +0100

    Rendering search.ejs

--- controllers/search.js
+++ controllers/search.js
@@ -1,8 +1,39 @@
 var express = require('express');
+var request = require('request-promise');
 var router = express.Router();
 
-router.get('/search', (req, res) =>{
-  res.render("../views/search.ejs")
-})
+const FT_API_KEY = process.env.FT_API_KEY;
+const apiUrl = "http://api.ft.com/content/search/v1?apiKey=" + FT_API_KEY;
+
+router.get('/search', (req,res) => {
+
+  const getSearch = {
+    url: apiUrl,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: {
+      "queryString" : req.query.search,
+      "queryContext" : {
+        "curations" : [ "ARTICLES"]
+      },
+      "resultContext" : {
+    		 "aspects" :[ "title","lifecycle","location","summary","editorial" ]
+    	}
+    },
+    json: true
+  };
+
+  request(getSearch)
+  .then(function (response) {
+    let articles = response.results[0].results;
+
+    res.render('search',
+     { title: 'FT HeadLines',
+      articles : articles} );
+  })
+});
+
 
 module.exports = router;
