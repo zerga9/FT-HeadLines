91945912a367752a816a362c38c9b7fd84c5fcd8
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-15 16:12:07 +0100

    Favicon implemented

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var index = require('./controllers/index');
 var search = require('./controllers/search');
+var favicon = require('serve-favicon')
 
 var app = express();
 
@@ -13,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '', 'favicon.ico')));
 
 app.use('/', index);
 app.use(search);
